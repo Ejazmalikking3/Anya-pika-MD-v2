e26ac6e8e6b492fe778288a459d568c84a633511
Ejking419 <117852372+Ejking419@users.noreply.github.com>
2023-03-20 12:30:56 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,19 +2,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.botname = "Farooq Baloch" 
+global.botname = "EJAZMALIK" 
 global.botLanguage = "default" // 𝗗𝗲𝗳𝗮𝘂𝗹𝘁 || 𝗘𝗻𝗴𝗹𝗶𝘀𝗵 || 𝗛𝗶𝗻𝗱𝗶 || 𝗔𝗿𝗮𝗯𝗶𝗰 \\
 global.author = "𝙋𝙄𝙆𝘼"  
-global.packname = "Farooq Baloch"  
+global.packname = "EJAZMALIK"  
 global.myweb = "https://github.com/PikaBotz" // You can put any link 
 global.waterMark = "×͜× King ✘ Pika™ ✓" // Your own watermark 
 global.headerText = "Anya by Pika" // text for header of menu         
-global.DocumentTitle = "MASTER⚔️Farooq" 
+global.DocumentTitle = "MASTER⚔️EJAZ" 
 
 
-global.ownername = "Farooq Baloch"   // Owner name //        
-global.ownernumber = ['923256073635'] 
-global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_Farooq Baloch" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
+global.ownername = "EJAZMALIK"   // Owner name //        
+global.ownernumber = ['923021115460'] 
+global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_EJAZMALIK" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
 global.ytname = "YT: Ytname"   // Yt chanel name //
 global.continent = "Asia" // your continent name 
 global.region = "India" // your country name
@@ -32,15 +32,15 @@ global.sudo = ['916900******','917355******']  // Co - Owners //
 global.prefa = ['-']
 
 //--------Random bot name for Info section--------\\
-name16 = "Farooq Kong"
-name17 = "Farooq King"
-name18 = "Farooq King"
+name16 = "EJAZMALIK"
+name17 = "EJAZMALIK"
+name18 = "EJAZMALIK"
 
 //--------Random footer message-----------\\
-footer1 = `Farooq King `
-footer2 = `Farooq King`
+footer1 = `EJAZMALIK`
+footer2 = `EJAZMALIK`
 
-//--------Left message "Nikal msg"--------\\
+//--------Left message "EJAZMALIK WHATSAPP BOT Nikal msg"--------\\
 nikalmsg1 = 'Ye galiye ye choubara, yaha dikhna na dubara 😚🖕🏻' 
 nikalmsg2 = 'Patt se headshot 🔫...Abey dil se bura lagta hai bhai 😂.'
 
@@ -61,7 +61,7 @@ global.mess = {
     success: 'Work done 𝗗𝗮𝗿𝗹𝗶𝗻𝗴❤️',
     admin: "Baby!\nThis command is only for 𝗔𝗱𝗺𝗶𝗻𝘀...So i can't process your command babe. 😑", // I used "\n" for change the line 
     botAdmin: 'First make me 𝗔𝗱𝗺𝗶𝗻!!! 😐',
-    owner: 'This command is only for my 𝗼𝘄𝗻𝗲𝗿..',
+    owner: 'This command is only for my 𝗼𝘄𝗻𝗲𝗿 EJAZMALIK..',
     group: "Baka !!!!!\nYou can't use this command in a 𝗽𝗿𝗶𝘃𝗮𝘁𝗲 𝗰𝗵𝗮𝘁 !😑",
     private: 'Such a baka !!\nThis command is only for 𝗽𝗿𝗶𝘃𝗮𝘁𝗲 𝗰𝗵𝗮𝘁𝘀. 🥲',
     bot: "This Feature Is Only For 𝗕𝗼𝘁... and you're not a 𝗕𝗼𝘁.",
